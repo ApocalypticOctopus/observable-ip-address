5ff64eca4be1bdbd8551213869f0d6f77fee8ebc
ApocalypticOctopus <apocalyptic.octopus@gmail.com>
2014-06-13 08:01:15 -0500

    Add logging and infinite loop for the change detector

--- observable-ip-address/observable-ip-address.cpp
+++ observable-ip-address/observable-ip-address.cpp
@@ -10,6 +10,192 @@
 #pragma comment(lib, "iphlpapi.lib")
 #pragma comment(lib, "ws2_32.lib")
 
+#define WORKING_BUFFER_SIZE 15000
+#define MAX_TRIES 3
+
+void ListIpAddresses(ULONG family = AF_INET)
+{
+	/* Declare and initialize variables */
+
+	DWORD dwSize = 0;
+	DWORD dwRetVal = 0;
+
+	unsigned int i = 0;
+
+	// Set the flags to pass to GetAdaptersAddresses
+	ULONG flags = GAA_FLAG_INCLUDE_PREFIX;
+
+	// default to unspecified address family (both)
+	//ULONG family = AF_UNSPEC;
+
+	LPVOID lpMsgBuf = NULL;
+
+	PIP_ADAPTER_ADDRESSES pAddresses = NULL;
+	ULONG outBufLen = 0;
+	ULONG Iterations = 0;
+
+	PIP_ADAPTER_ADDRESSES pCurrAddresses = NULL;
+	PIP_ADAPTER_UNICAST_ADDRESS pUnicast = NULL;
+	PIP_ADAPTER_ANYCAST_ADDRESS pAnycast = NULL;
+	PIP_ADAPTER_MULTICAST_ADDRESS pMulticast = NULL;
+	IP_ADAPTER_DNS_SERVER_ADDRESS *pDnServer = NULL;
+	IP_ADAPTER_PREFIX *pPrefix = NULL;
+
+	//if (atoi(argv[1]) == 4)
+	//	family = AF_INET;
+	//else if (atoi(argv[1]) == 6)
+	//	family = AF_INET6;
+
+	printf("Calling GetAdaptersAddresses function with family = ");
+	if (family == AF_INET)
+		printf("AF_INET\n");
+	if (family == AF_INET6)
+		printf("AF_INET6\n");
+	if (family == AF_UNSPEC)
+		printf("AF_UNSPEC\n\n");
+
+	// Allocate a 15 KB buffer to start with.
+	outBufLen = WORKING_BUFFER_SIZE;
+
+	do {
+
+		pAddresses = (IP_ADAPTER_ADDRESSES *)malloc(outBufLen);
+		if (pAddresses == NULL) {
+			printf
+				("Memory allocation failed for IP_ADAPTER_ADDRESSES struct\n");
+			exit(1);
+		}
+
+		dwRetVal =
+			GetAdaptersAddresses(family, flags, NULL, pAddresses, &outBufLen);
+
+		if (dwRetVal == ERROR_BUFFER_OVERFLOW) {
+			free(pAddresses);
+			pAddresses = NULL;
+		}
+		else {
+			break;
+		}
+
+		Iterations++;
+
+	} while ((dwRetVal == ERROR_BUFFER_OVERFLOW) && (Iterations < MAX_TRIES));
+
+	if (dwRetVal == NO_ERROR) {
+		// If successful, output some information from the data we received
+		pCurrAddresses = pAddresses;
+		while (pCurrAddresses) {
+			printf("\tLength of the IP_ADAPTER_ADDRESS struct: %ld\n",
+				pCurrAddresses->Length);
+			printf("\tIfIndex (IPv4 interface): %u\n", pCurrAddresses->IfIndex);
+			printf("\tAdapter name: %s\n", pCurrAddresses->AdapterName);
+
+			pUnicast = pCurrAddresses->FirstUnicastAddress;
+			if (pUnicast != NULL) {
+				for (i = 0; pUnicast != NULL; i++)
+					pUnicast = pUnicast->Next;
+				printf("\tNumber of Unicast Addresses: %d\n", i);
+			}
+			else
+				printf("\tNo Unicast Addresses\n");
+
+			pAnycast = pCurrAddresses->FirstAnycastAddress;
+			if (pAnycast) {
+				for (i = 0; pAnycast != NULL; i++)
+					pAnycast = pAnycast->Next;
+				printf("\tNumber of Anycast Addresses: %d\n", i);
+			}
+			else
+				printf("\tNo Anycast Addresses\n");
+
+			pMulticast = pCurrAddresses->FirstMulticastAddress;
+			if (pMulticast) {
+				for (i = 0; pMulticast != NULL; i++)
+					pMulticast = pMulticast->Next;
+				printf("\tNumber of Multicast Addresses: %d\n", i);
+			}
+			else
+				printf("\tNo Multicast Addresses\n");
+
+			pDnServer = pCurrAddresses->FirstDnsServerAddress;
+			if (pDnServer) {
+				for (i = 0; pDnServer != NULL; i++)
+					pDnServer = pDnServer->Next;
+				printf("\tNumber of DNS Server Addresses: %d\n", i);
+			}
+			else
+				printf("\tNo DNS Server Addresses\n");
+
+			printf("\tDNS Suffix: %wS\n", pCurrAddresses->DnsSuffix);
+			printf("\tDescription: %wS\n", pCurrAddresses->Description);
+			printf("\tFriendly name: %wS\n", pCurrAddresses->FriendlyName);
+
+			if (pCurrAddresses->PhysicalAddressLength != 0) {
+				printf("\tPhysical address: ");
+				for (i = 0; i < (int)pCurrAddresses->PhysicalAddressLength;
+					i++) {
+					if (i == (pCurrAddresses->PhysicalAddressLength - 1))
+						printf("%.2X\n",
+						(int)pCurrAddresses->PhysicalAddress[i]);
+					else
+						printf("%.2X-",
+						(int)pCurrAddresses->PhysicalAddress[i]);
+				}
+			}
+			printf("\tFlags: %ld\n", pCurrAddresses->Flags);
+			printf("\tMtu: %lu\n", pCurrAddresses->Mtu);
+			printf("\tIfType: %ld\n", pCurrAddresses->IfType);
+			printf("\tOperStatus: %ld\n", pCurrAddresses->OperStatus);
+			printf("\tIpv6IfIndex (IPv6 interface): %u\n",
+				pCurrAddresses->Ipv6IfIndex);
+			printf("\tZoneIndices (hex): ");
+			for (i = 0; i < 16; i++)
+				printf("%lx ", pCurrAddresses->ZoneIndices[i]);
+			printf("\n");
+
+			printf("\tTransmit link speed: %I64u\n", pCurrAddresses->TransmitLinkSpeed);
+			printf("\tReceive link speed: %I64u\n", pCurrAddresses->ReceiveLinkSpeed);
+
+			pPrefix = pCurrAddresses->FirstPrefix;
+			if (pPrefix) {
+				for (i = 0; pPrefix != NULL; i++)
+					pPrefix = pPrefix->Next;
+				printf("\tNumber of IP Adapter Prefix entries: %d\n", i);
+			}
+			else
+				printf("\tNumber of IP Adapter Prefix entries: 0\n");
+
+			printf("\n");
+
+			pCurrAddresses = pCurrAddresses->Next;
+		}
+	}
+	else {
+		printf("Call to GetAdaptersAddresses failed with error: %d\n",
+			dwRetVal);
+		if (dwRetVal == ERROR_NO_DATA)
+			printf("\tNo addresses were found for the requested parameters\n");
+		else {
+
+			if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+				FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+				NULL, dwRetVal, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+				// Default language
+				(LPTSTR)& lpMsgBuf, 0, NULL)) {
+				printf("\tError: %s", lpMsgBuf);
+				LocalFree(lpMsgBuf);
+				if (pAddresses)
+					free(pAddresses);
+				exit(1);
+			}
+		}
+	}
+
+	if (pAddresses) {
+		free(pAddresses);
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	OVERLAPPED overlap;
@@ -18,19 +204,25 @@ int _tmain(int argc, _TCHAR* argv[])
 	HANDLE hand = NULL;
 	overlap.hEvent = WSACreateEvent();
 
-	ret = NotifyAddrChange(&hand, &overlap);
-
-	if (ret != NO_ERROR)
+	while (true)
 	{
-		if (WSAGetLastError() != WSA_IO_PENDING)
+		ret = NotifyAddrChange(&hand, &overlap);
+
+		if (ret != NO_ERROR)
 		{
-			printf("NotifyAddrChange error...%d\n", WSAGetLastError());
-			return 1;
+			if (WSAGetLastError() != WSA_IO_PENDING)
+			{
+				printf("NotifyAddrChange error...%d\n", WSAGetLastError());
+				return 1;
+			}
+		}
+
+		if (WaitForSingleObject(overlap.hEvent, INFINITE) == WAIT_OBJECT_0)
+		{
+			ListIpAddresses();
 		}
 	}
 
-	if (WaitForSingleObject(overlap.hEvent, INFINITE) == WAIT_OBJECT_0)
-		printf("IP Address table changed..\n");
-
 	return 0;
 }
+

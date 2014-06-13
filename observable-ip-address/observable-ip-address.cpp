110a215a137ebe1f0c70e86cfa59fa33d059f8f5
ApocalypticOctopus <apocalyptic.octopus@gmail.com>
2014-06-13 07:54:29 -0500

    Add code that hangs until the IP address changes

--- observable-ip-address/observable-ip-address.cpp
+++ observable-ip-address/observable-ip-address.cpp
@@ -2,10 +2,35 @@
 //
 
 #include "stdafx.h"
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#include <windows.h>
 
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	OVERLAPPED overlap;
+	DWORD ret;
+
+	HANDLE hand = NULL;
+	overlap.hEvent = WSACreateEvent();
+
+	ret = NotifyAddrChange(&hand, &overlap);
+
+	if (ret != NO_ERROR)
+	{
+		if (WSAGetLastError() != WSA_IO_PENDING)
+		{
+			printf("NotifyAddrChange error...%d\n", WSAGetLastError());
+			return 1;
+		}
+	}
+
+	if (WaitForSingleObject(overlap.hEvent, INFINITE) == WAIT_OBJECT_0)
+		printf("IP Address table changed..\n");
+
 	return 0;
 }
-
